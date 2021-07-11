1001c0818432860b538ad8bd69a7ef306d361c62
Nitin286roxs <nitinashu1995@gmail.com>
2021-07-11 09:03:11 +0530

    Naive Classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,20 +14,26 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
-
+from sklearn.naive_bayes import GaussianNB
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+#########################################################
+### Train Gaussian Classifier ###
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
 
 #########################################################
-### your code goes here ###
-
-
+### Predict Features ###
+pred = clf.predict(features_test)
 #########################################################
 
+#########################################################
+### Calculate Accuracy ###
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+print(accuracy)
+#########################################################
 
